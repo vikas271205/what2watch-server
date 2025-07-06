daee2d35b7cbe09eee660147daee9e7023c558d8
vikas271205 <vikasjakhar271205@example.com>
2025-07-06 23:31:48 +0530

    Allowed core rules for frontend

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://what2watch.web.app"],
+    origin: "https://what2watch-271205.web.app", // allow Firebase app
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
 app.use(express.json());
