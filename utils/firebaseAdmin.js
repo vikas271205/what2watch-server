07282cb83f8ee5a3f4aa27cdf27b65e52b948f2c
vikas271205 <vikasjakhar271205@example.com>
2025-07-08 01:19:41 +0530

    Use FIREBASE_SERVICE_ACCOUNT env var instead of file

--- utils/firebaseAdmin.js
+++ utils/firebaseAdmin.js
@@ -1,20 +1,7 @@
 import admin from "firebase-admin";
-import path from "path";
-import { fileURLToPath } from "url";
-import fs from "fs";
-
-// Resolve __dirname in ES module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Path to your Firebase service account key
-const serviceAccountPath = path.resolve(__dirname, "../../firebase-admin.json");
 
 if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../firebase-admin.json")));
-
-
-
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
